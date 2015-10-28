e0e96f7c4a8b47a1147edb045509c38b295ea184
suman <suman@wrctechnologies.com>
2015-10-28 12:32:36 +0530

    commit again

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 //QB Account params
 var QBPARAMS = {
-	app_id      : 29923,
-	auth_key    : 'LMqWgbkdjhOJz2E',
-	auth_secret : 'Fjf2HP87Ouk26dK'
+	app_id      : 28654,
+	auth_key    : 'mudQbb8SsH3y-ge',
+	auth_secret : '8jbZym59NHNVTeD'
 }
 
 //Chat params
 var CHAT = {
 	server      : 'chat.quickblox.com',
 	bosh_url    : 'http://chat.quickblox.com:5280',
-	roomJID     : '29923_testing@muc.chat.quickblox.com'
+	roomJID     : '28654_56024609a28f9ae88f0053e7@muc.chat.quickblox.com'
 }
