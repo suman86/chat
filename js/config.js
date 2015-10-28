c73aff6a0e88bbcb3c3da276b35d6c7384376893
suman <suman@wrctechnologies.com>
2015-10-28 10:33:00 +0530

    initial commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+//QB Account params
+var QBPARAMS = {
+	app_id      : 29923,
+	auth_key    : 'LMqWgbkdjhOJz2E',
+	auth_secret : 'Fjf2HP87Ouk26dK'
+}
+
+//Chat params
+var CHAT = {
+	server      : 'chat.quickblox.com',
+	bosh_url    : 'http://chat.quickblox.com:5280',
+	roomJID     : '29923_testing@muc.chat.quickblox.com'
+}
